a4d4d76c5f94bd7bff77958703fda35de4ffcac3
emiridbest <Udogwuemiri@gmail.com>
2023-04-18 23:40:29 +0100

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,8 @@
+export { default as Welcome } from "./Welcome";
+export { default as ManufactureItem } from "./ManufactureItem";
+export { default as Navbar } from "./Navbar";
+export { default as SellItem } from "./SellItem";
+export { default as ShipToDistributor } from "./ShipToDistributor";
+export { default as ShipToRetailer } from "./ShipToRetailer";
+export { default as TrackItem } from "./TrackItem";
+export { default as AssignRole } from "./AssignRole";
